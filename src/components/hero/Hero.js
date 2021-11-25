cd6758cf1a03aa47af8e8f00b741369e5703590b
Stewar Marin <stewar1020@gmail.com>
2021-11-25 13:54:36 -0500

    Added second router

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,7 @@
+export const Hero = () => {
+  return (
+    <div>
+      <h1>Hero Screen</h1>
+    </div>
+  );
+};
--- src/components/routers/AppRouter.js
+++ src/components/routers/AppRouter.js
@@ -1,22 +1,14 @@
-import React from 'react';
 import { Route, Routes } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
-import { DcScreen } from '../dc/DcScreen';
 import { LoginScreen } from '../login/LoginScreen';
-import { MarvelScreen } from '../marvel/MarvelScreen';
-import { SearchScreen } from '../search/SearchScreen';
-import { Navbar } from '../ui/Navbar';
+import { DashboardRoutes } from './DashboardRoutes';
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
-        <Route path='/' element={<MarvelScreen />} />
-        <Route path='/marvel' element={<MarvelScreen />} />
-        <Route path='/dc' element={<DcScreen />} />
-        <Route path='/search' element={<SearchScreen />} />
         <Route path='/login' element={<LoginScreen />} />
+        <Route path='/*' element={<DashboardRoutes />} />
       </Routes>
     </BrowserRouter>
   );
