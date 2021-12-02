96ed6c11363b9aa613b2769b293b9cda04856932
Stewar Marin <stewar1020@gmail.com>
2021-12-02 17:02:16 -0500

    Added styles for hero & added useMemo hook

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,18 +1,59 @@
-import { useParams, Navigate } from 'react-router-dom';
+import { useMemo } from 'react';
+import { useParams, Navigate, useNavigate } from 'react-router-dom';
 import { getHeroById } from '../../selectors/getHeroById';
 
 export const Hero = () => {
   const { heroId } = useParams();
-  const hero = getHeroById(heroId);
+  const navigate = useNavigate();
+  const hero = useMemo(() => {
+    getHeroById(heroId);
+  }, [heroId]);
+  const { alter_ego, characters, superhero, first_appearance, id, publisher } =
+    hero;
 
   if (!hero) {
     return <Navigate to='/marvel' />;
   }
 
+  const handleReturn = () => {
+    navigate(-1);
+  };
+
   return (
-    <div>
-      <h1>Hero Screen</h1>
-      <p>{hero.superhero}</p>
+    <div className='row mt-5'>
+      <div className='col-4'>
+        <img
+          className='img-thumbnail'
+          src={`/assets/${id}.jpg`}
+          alt={superhero}
+        />
+      </div>
+      <div className='col-8'>
+        <h3>{superhero}</h3>
+        <ul className='list-group'>
+          <li className='list-group-item'>
+            <b>Alter Ego: </b>
+            {alter_ego}
+          </li>
+          <li className='list-group-item'>
+            <b>Publisher: </b>
+            {publisher}
+          </li>
+          <li className='list-group-item'>
+            <b>First appearence: </b>
+            {first_appearance}
+          </li>
+          <li className='list-group-item'>
+            <b>Characters: </b>
+            {characters}
+          </li>
+        </ul>
+        <div className='d-grid'>
+          <button className='btn btn-outline-info mt-2' onClick={handleReturn}>
+            Back
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
