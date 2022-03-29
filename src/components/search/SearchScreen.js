5dcc08ce36e77585d6a698c2f271474c40498733
Stewar Marin <stewar1020@gmail.com>
2022-03-28 19:45:33 -0500

    Added search bar functionality

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useForm } from '../../hooks/useForm';
+import queryString from 'query-string';
+import { getHerosByName } from '../../selectors/getHerosByName';
+import { HeroCard } from '../hero/HeroCard';
 
 export const SearchScreen = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { hero = '' } = queryString.parse(location.search);
   const [formValues, handleInputChange] = useForm({
-    searchText: '',
+    searchText: hero,
   });
 
+  const heroesFilter = useMemo(() => getHerosByName(hero), [hero]);
   const { searchText } = formValues;
 
   const handleSearch = (e) => {
     e.preventDefault();
-    console.log(searchText);
+    navigate(`?hero=${searchText}`);
   };
   return (
     <>
@@ -34,6 +42,23 @@ export const SearchScreen = () => {
               Search
             </button>
           </form>
+        </div>
+
+        <div className='col-7'>
+          <h4>Results</h4>
+          <hr />
+          {hero === '' ? (
+            <div className='alert alert-primary'> Search a hero</div>
+          ) : (
+            heroesFilter.length === 0 && (
+              <div className='alert alert-danger'>
+                Whoops... We don't found anything with "{hero}"
+              </div>
+            )
+          )}
+          {heroesFilter.map((hero) => (
+            <HeroCard key={hero.id} {...hero} />
+          ))}
         </div>
       </div>
     </>
